3801e6e29c52e5febea6e59dd622678c2b733cf4
Zmachinskiy <yuri.zmachinsky@gmail.com>
2017-06-29 11:28:16 +0300

    register page commit

---
+++ client/src/app/components/register/register.component.ts
@@ -0,0 +1,98 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  form:FormGroup;
+
+
+
+  constructor(private formBuilder: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm(){
+    this.form=this.formBuilder.group(
+      {
+        email:['',Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(30),
+          this.validateEmail
+        ])],
+        username:['',Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(30),
+          this.validateUsername
+        ])],
+        password:['',Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(30),
+          this.validatePassword
+        ])],
+        confirmPassword:['',Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(30),
+
+        ])],
+      },
+      {
+        validator: this.matchingPassword('password','confirmPassword')
+      }
+    )
+  }
+
+  //custom validations
+  validateEmail(controls){
+    const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if(regExp.test(controls.value)){
+      return null;
+    }else {
+      return {validateEmail:true}
+    }
+  }
+
+  validateUsername(controls){
+    const regExp = new RegExp(/^[a-z0-9_-]{5,30}$/);
+    if(regExp.test(controls.value)){
+      return null;
+    }else return {validateUsername:true}
+  }
+
+  validatePassword(controls){
+    const regExp = new RegExp(/^[a-z0-9_-]{5,30}$/);
+    if(regExp.test(controls.value)){
+      return null;
+    }else return {validatePassword:true}
+  }
+
+  matchingPassword(password,confirmPassword){
+    return (group:FormGroup)=>{
+      if(group.controls[password].value===group.controls[confirmPassword].value){
+        return null;
+      }else {
+        return {matchingPassword:true}
+      }
+    }
+  }
+
+
+
+
+  onRegistrySubmit(){
+    console.log(this.form);
+  }
+
+
+  ngOnInit() {
+  }
+
+}
--- models/user.js
+++ models/user.js
@@ -24,16 +24,61 @@ let validEmailChecker = (email)=>{
   }
 };
 
+let usernameLengthChecker = (username)=>{
+    if(!username){return false}
+    else {
+        if(username.length<5 || username.length>30){return false}
+        else return true;
+    }
+};
+
+let validUsernameChecker = (username)=>{
+    if(!username) return false;
+    else {
+        const regExp = new RegExp(/^[a-z0-9_-]{5,30}$/);
+        return regExp.test(username);
+    }
+};
+
+let passwordLengthCheck = (password)=>{
+    if(!password){
+        return false
+    }else {
+        if(password.length<5 || password.length>30){
+            return false
+        }else return true
+    }
+};
+
+
+let validPassword = (password)=>{
+    if(!password){
+        return false;
+    }else {
+        const regExp = new RegExp(/^[a-z0-9_-]{5,30}$/);
+        return regExp.test(password)
+    }
+};
+
+const usernameValidators=[
+    {validator: usernameLengthChecker,message:"Username must be at least 5 characters but no more than 30"},
+    {validator: validUsernameChecker,message:"Username must contain a correct characters"}
+];
 
 const emailValidators = [
     {validator: emailLengthChecker,message:'E-mail must be at least 5 characters but no more than 30'},
-    {validator:validEmailChecker,message:'E-mail must contant a right characters'}
+    {validator:validEmailChecker,message:'E-mail must contain a correct characters'}
+];
+
+const passwordValidators = [
+    {validator: passwordLengthCheck,message:'Password must be at least 5 characters but no more than 30 '},
+    {validator: validPassword,message:'Password must contain a correct characters'}
 ];
 
 const userSchema = new Schema({
     email:{type:String,required:true,unique:true,lowercase:true,validate:emailValidators},
-    username:{type:String,required:true,unique:true,lowercase:true},
-    password:{type:String,required:true,}
+    username:{type:String,required:true,unique:true,lowercase:true,validate:usernameValidators},
+    password:{type:String,required:true,validate:passwordValidators}
 
 });
 
